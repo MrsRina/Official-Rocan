d62e32adbbf4aeb408876ce70f4673afc3ccd2b2
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2021-02-05 16:41:44 -0300

    Auto Armour update

--- src/main/java/me/rina/rocan/client/module/combat/ModuleAutoArmour.java
+++ src/main/java/me/rina/rocan/client/module/combat/ModuleAutoArmour.java
@@ -4,32 +4,30 @@ import me.rina.rocan.api.event.impl.EventStage;
 import me.rina.rocan.api.module.Module;
 import me.rina.rocan.api.module.impl.ModuleCategory;
 import me.rina.rocan.api.util.item.SlotUtil;
-import me.rina.rocan.api.util.math.TimeUtil;
 import me.rina.rocan.client.event.client.ClientTickEvent;
+import me.rina.turok.util.TurokTick;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.ClickType;
 import net.minecraft.item.Item;
 import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
 
 public class ModuleAutoArmour extends Module {
-
   private static final Item[] HELMETS = {Items.DIAMOND_HELMET, Items.IRON_HELMET, Items.GOLDEN_HELMET, Items.CHAINMAIL_HELMET, Items.LEATHER_HELMET};
   private static final Item[] CHESTPLATES = {Items.DIAMOND_CHESTPLATE, Items.IRON_CHESTPLATE, Items.GOLDEN_CHESTPLATE, Items.CHAINMAIL_CHESTPLATE, Items.LEATHER_CHESTPLATE};
   private static final Item[] LEGGINGS = {Items.DIAMOND_LEGGINGS, Items.IRON_LEGGINGS, Items.GOLDEN_LEGGINGS, Items.CHAINMAIL_LEGGINGS, Items.LEATHER_LEGGINGS};
   private static final Item[] BOOTS = {Items.DIAMOND_BOOTS, Items.IRON_BOOTS, Items.GOLDEN_BOOTS, Items.CHAINMAIL_BOOTS, Items.LEATHER_BOOTS};
-  private TimeUtil timer = new TimeUtil();
+  private TurokTick timer = new TurokTick();
 
   public ModuleAutoArmour() {
-    super("Auto Armour", "autoarmour", "Automatically places armour on.", ModuleCategory.Combat);
+    super("Auto Armour", "AutoArmour", "Automatically places armour on.", ModuleCategory.Combat);
   }
-
 
   @Listener
   public void onTick(ClientTickEvent event) {
     if (mc.player == null) return;
     if(event.getStage() == EventStage.Pre) {
       int selectedSlotId = -1;
-      if (timer.reach((long) 100.69420)) {
+      if (timer.isPassedMS((long) 100.69420)) {
         if (mc.player.inventory.armorItemInSlot(2).getItem() == Items.AIR) {
           for (Item item : CHESTPLATES) {
             int slotId = SlotUtil.findItemSlotFromHotBar(item);
