049190ca2bd885b337b579876e46d4cccfabc172
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2021-02-10 14:10:18 -0300

    Underground github

--- src/main/java/me/rina/rocan/client/module/render/ModuleFullBright.java
+++ src/main/java/me/rina/rocan/client/module/render/ModuleFullBright.java
@@ -12,6 +12,10 @@ import team.stiff.pomelo.impl.annotated.handler.annotation.Listener;
 
 import java.util.Objects;
 
+/**
+ * Jake! not make me insane! don't put 1k in gamma!!! it glicth all game light!!!!
+ * lol
+ */
 @Registry(name = "Full Bright", tag = "FullBright", description = "Changes the brightness of the client.", category = ModuleCategory.RENDER)
 public class ModuleFullBright extends Module {
     @Override
